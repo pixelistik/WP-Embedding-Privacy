a3b52b5d13129690e0cd97ce90e98ff61104a26c
pixelistik <code@pixelistik.de>
2011-02-09 02:15:07 +0100

    Clean up, better order and consistent whitespace.

--- WP-embedding-privacy/WP-embedding-privacy.php
+++ WP-embedding-privacy/WP-embedding-privacy.php
@@ -13,34 +13,41 @@ License: GPL2
 /* Thank you, http://bjornery.com/web/hacking-oembed/ */
 $youtubeparser = new youtubeParse;
 class youtubeParse {
-	function parse($return, $data, $url)
-	{
-		if ($data->type=='video') {
-			$pre = '<div class="WP-embedding-privacy-container">
-					<a href="'.$url.'" id="trigger">
-						<img src="'.$data->thumbnail_url.'" />
-					</a>
-				<script type="text/plain">
-			';
-			$post = '</script>
-				</div>
-			';
-			$return = $pre . $return . $post;
-		}
-		return $return;
-	}
+	/**
+	 * Constructor, registers javascripts and our filter.
+	 */
 	function youtubeParse()
 	{
 		wp_enqueue_script('jquery');
 		
 		wp_register_script(
 			'WP-embedding-privacy',
-			WP_PLUGIN_URL . '/WP-embedding-privacy/js/WP-embedding-privacy.js',
+			WP_PLUGIN_URL.'/WP-embedding-privacy/js/WP-embedding-privacy.js',
 			array('jquery')
 		);
 		wp_enqueue_script('WP-embedding-privacy');
 
 		add_filter('oembed_dataparse',array(&$this,'parse'),10,3);
+	}
+	
+	/**
+	 * The filter function. Works on video type embeds only.
+	 */
+	function parse($return, $data, $url)
+	{
+		if ($data->type=='video') {
+			$pre='<div class="WP-embedding-privacy-container">
+					<a href="'.$url.'" id="trigger">
+						<img src="'.$data->thumbnail_url.'" />
+					</a>
+				<script type="text/plain">
+			';
+			$post='</script>
+				</div>
+			';
+			$return=$pre.$return.$post;
+		}
+		return $return;
 	}
 }
 ?>
